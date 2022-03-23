730b4d38a355335216b0279473af18a045b5414d
Alexis <alexisjlino@gmail.com>
2022-03-23 20:10:03 -0300

    TP1 Final

--- backend/bin/activate.fish
+++ backend/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/alumno/Desktop/Programacion/backend"
+set -gx VIRTUAL_ENV "/home/alumno/Desktop/Programacion-I/backend"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
