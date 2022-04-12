1fcae6b046768f2137ea543130c9702e4f05b218
Alexis <alexisjlino@gmail.com>
2022-04-12 19:53:40 -0300

    Trabajo en clase, DB

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,10 @@ from main import create_app
 import os
 app = create_app()
 app.app_context().push()
+from main import db
+
 if __name__ == "__main__":
+    db.create_all()
     app.run(debug=True,port=os.getenv("PORT"))
 
 
--- backend/main/__init__.py
+++ backend/main/__init__.py
@@ -4,9 +4,19 @@ from flask import Flask
 from dotenv import load_dotenv
 from flask_restful import Api
 import main.resources as resources
+from flask_sqlalchemy import SQLAlchemy
 api = Api()
 #Mètodo que inicializa todos los mòdulos
+db = SQLAlchemy()
 def create_app():
+    if not os.path.exists(os.getenv('DB_PATH')+os.getenv('DB_NAME')):
+        os.mknod(os.getenv('DB_PATH')+os.getenv('DB_NAME'))
+
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    #Url de configuración de base de datos
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////'+os.getenv('DB_PATH')+os.getenv('DB_NAME')
+    db.init_app(app)
+
     #Inicializa Flask
     app = Flask(__name__)
     #Cargar variables de entorno 
