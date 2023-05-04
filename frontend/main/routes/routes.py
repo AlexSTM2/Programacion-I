3d17225904082aafa933112d7fce7c42fc04d9dd
Alexis <you@example.com>
2023-05-03 21:41:23 -0300

    Datos de calificación y poemas públicos

--- frontend/main/routes/routes.py
+++ frontend/main/routes/routes.py
@@ -80,7 +80,7 @@ def ver_poema(id, jwt=None):
 @app.route('/ver-poema-usuario/<int:id>')
 def ver_poema_usuario(id):
     jwt = f.obtener_jwt()
-    if jwt == None: #NameError("Token has expired"):
+    if jwt == None and jwt != TypeError("Token has expired"):
         return redirect(url_for('main.index'))
     else:
         usuario = f.obtener_usuario(f.obtener_id())
@@ -90,14 +90,24 @@ def ver_poema_usuario(id):
         return render_template('datos_poema_usuario.html', poema = poema, usuario=usuario)
 
 
-@app.route('/ver-calif-publico')
-def ver_calif_publico(): 
-    return render_template('ver_calif_publico.html')
+@app.route('/ver_calif_publico/<int:id>/<int:id_calif>')
+def ver_calif_publico(id, id_calif):
+    jwt = f.obtener_jwt()
+    if jwt != None and jwt != TypeError("Token has expired"):
+        return redirect(url_for('main.ver_calif_usuario', id=id, id_calif=id_calif))
+    else:
+        poema = f.obtener_poema(id, without_token=True)
+        poema = f.obtener_json(poema)
+        calif = f.obtener_calificacion(id_calif)
+        calif = f.obtener_json(calif)
+        print("Calif: ", calif)
+        print("Poema: ", poema)
+        return render_template('ver_calif_publico.html', poema = poema, calificacion = calif)
 
 @app.route('/ver_calif_usuario/<int:id>/<int:id_calif>')
 def ver_calif_usuario(id, id_calif):
     jwt = f.obtener_jwt()
-    if jwt != None:
+    if jwt != None and jwt != TypeError("Token has expired"):
         poema = f.obtener_poema(id)
         poema = f.obtener_json(poema)
         id_usuario = f.obtener_id()
@@ -173,13 +183,17 @@ def calificar(id_poema):
         usuario = f.obtener_json(usuario)
         poema = f.obtener_poema(id_poema)
         poema = f.obtener_json(poema)
+
         if(request.method == "POST"):
             puntaje = request.form.get('check_form')
             comentario = request.form.get('form_comentario')
-            if puntaje != "" and comentario != "":
+            if puntaje != "" and comentario != "" and poema["Autor"] != usuario["Nombre"]:
                 data = {"comentario": comentario, "puntaje": puntaje, "poema_id": id_poema}
                 headers = f.obtener_headers(without_token=False)
                 response = requests.post(f'{current_app.config["API_URL"]}/calificaciones', json=data, headers=headers)
+            
+            elif poema["Autor"] == usuario["Nombre"]:
+                return render_template('calificar.html', usuario = usuario, poema=poema, error = "No puedes calificar tu propio poema")
             
             if response.ok:
                 resp = make_response(redirect(url_for('main.index_usr')))
